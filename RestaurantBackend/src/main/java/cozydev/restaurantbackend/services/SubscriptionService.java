7b02052a5649da70950aa1b6ecd72c6972e99ae4
yassinops <yassinfaouar@gmail.com>
2024-05-04 01:32:00 +0100

    Subs

--- RestaurantBackend/src/main/java/cozydev/restaurantbackend/services/SubscriptionService.java
+++ RestaurantBackend/src/main/java/cozydev/restaurantbackend/services/SubscriptionService.java
@@ -5,17 +5,20 @@ import cozydev.restaurantbackend.repositories.SubscriptionRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SubscriptionService {
     private final SubscriptionRepository subscriptionRepository;
-
     public SubscriptionService(SubscriptionRepository subscriptionRepository) {
         this.subscriptionRepository = subscriptionRepository;
     }
 
-
     public List<Subscription> getAllSubs(){
         return subscriptionRepository.findAll();
+    }
+
+    public Optional<Subscription> getSubsById(long id) {
+        return subscriptionRepository.findById(id);
     }
 }
