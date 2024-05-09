773eb28280a40fda1d27e492f68c3cb34f4eaecb
TEBINI Safwen <153730465+FaintShadow@users.noreply.github.com>
2024-05-09 12:56:04 +0100

    Delete AngularFrontend directory

--- AngularFrontend/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import {SubscriptionComponent} from "./subscription/subscription.component";
-
-export const routes: Routes = [
-  {path:'',redirectTo:'subs',pathMatch:'full'},
-  {path:'subs',component:SubscriptionComponent}
-];
