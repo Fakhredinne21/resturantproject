bc2354b77c8e97492531defbe95bb40942c3d387
mohamed amine idoudi <mohamedamineidoudi@gmail.com>
2024-05-10 22:21:40 +0100

    angular !

---
+++ RestaurantFrontend/src/app/dashborde/dashborde.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashbordeRoutingModule } from './dashborde-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DashbordeRoutingModule
+  ]
+})
+export class DashbordeModule { }
