58803586ede39f83a400558f71fde906bb846dd4
mohamed amine idoudi <mohamedamineidoudi@gmail.com>
2024-05-10 23:20:12 +0100

    login

--- RestaurantFrontend/src/app/login/login.component.ts
+++ RestaurantFrontend/src/app/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+
+
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  emailControl = new FormControl('');
+  passwordControl = new FormControl('');
+  formBuilder = new FormBuilder();
+  loginform: FormGroup = this.formBuilder.group({
+    email: this.emailControl,
+    password: this.passwordControl
+  });
+  onSubmit(){
+    //implement service
+  }
 
 }
