ca5ebad5cfd9d79e657954a18f06cc84ed536c73
FaintShadow <153730465+FaintShadow@users.noreply.github.com>
2024-05-22 13:32:19 +0100

    Login styling finalized, removed rightside component

--- RestaurantFrontend/src/app/login/login.module.ts
+++ RestaurantFrontend/src/app/login/login.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import {ReactiveFormsModule} from "@angular/forms";
-import { RightsideComponent } from './rightside/rightside.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RightsideComponent
   ],
     imports: [
         CommonModule,
@@ -18,8 +16,7 @@ import { RightsideComponent } from './rightside/rightside.component';
         ReactiveFormsModule
     ],
   exports: [
-    LoginComponent,
-    RightsideComponent
+    LoginComponent
   ]
 })
 export class LoginModule { }
