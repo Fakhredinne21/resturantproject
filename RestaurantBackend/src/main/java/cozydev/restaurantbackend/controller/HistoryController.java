26a14909475a26331d98966c3359d1eba166829e
Naceur <naceur@F4-A4-75-EC-44-7E.home>
2024-05-29 15:31:38 +0100

    !

--- RestaurantBackend/src/main/java/cozydev/restaurantbackend/controller/HistoryController.java
+++ RestaurantBackend/src/main/java/cozydev/restaurantbackend/controller/HistoryController.java
@@ -9,23 +9,27 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/History")
 @RequiredArgsConstructor
 public class HistoryController {
 
-    private final HistoryRepository historyRepository ;
     private final HistoryService historyService;
 
     @PostMapping
     public void saveHistory(History history){
-        historyRepository.save(history);
         historyService.save(history);
     }
 
     @GetMapping("/date/{date}")
     public Long getHistoriesByDate(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         return historyService.getHistoriesByDate(date);
+    }
+
+    @GetMapping("/history/{userId}")
+    public List<History> getHistoriesByUserId(@PathVariable Long userId) {
+        return historyService.getHistoriesByUserId(userId);
     }
 }
--- RestaurantBackend/src/main/java/cozydev/restaurantbackend/services/HistoryService.java
+++ RestaurantBackend/src/main/java/cozydev/restaurantbackend/services/HistoryService.java
@@ -2,6 +2,9 @@ package cozydev.restaurantbackend.services;
 
 import cozydev.restaurantbackend.model.History;
 import cozydev.restaurantbackend.repositories.HistoryRepository;
+import cozydev.restaurantbackend.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +13,13 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class HistoryService {
 
+public class HistoryService {
+    private final UserRepository userRepository;
     private final HistoryRepository historyRepository ;
 
-    public void saveHistroy(History history){
-        historyRepository.save(history);
-    }
+
+
 
     public Long getHistoriesByDate(LocalDate date) {
         return  historyRepository.findAll().stream()
@@ -26,5 +29,14 @@ public class HistoryService {
 
     public void save(History history) {
         historyRepository.save(history);
+    }
+
+    public List<History> getHistoriesByUserId(Long userId) {
+        return historyRepository.findAll().stream()
+                .filter(history ->
+                        history.getSender().getUserId().equals(userId)
+                        ||
+                        history.getReciever().getUserId().equals(userId))
+                .toList();
     }
 }
